9ef0e8264f5ae3172f1739ddea500189c45e52e6
a.grinevich <Toha909570!>
2020-10-30 14:02:29 +0200

    Lab done

--- HomeWork2.py
+++ HomeWork2.py
@@ -1,12 +1,12 @@
-import json
+# import json
 
 sample_dict = {
-   "class_a":{
-      "student1":{
-         "name":"Misha",
-         "marks":{
-            "math":90,
-            "history":85
+   "class_a": {
+      "student1": {
+         "name": "Misha",
+         "marks": {
+            "math": 90,
+            "history": 85
          }
       }
    }
@@ -17,19 +17,19 @@ print(sample_dict["class_a"]["student1"]["name"])
 print(sample_dict["class_a"]["student1"]["marks"]["history"])
 
 sample_dict["class_a"]["student2"] = {
-         "name":"Petya",
-         "marks":{
-            "math":14,
-            "history":88
+         "name": "Petya",
+         "marks": {
+            "math": 14,
+            "history": 88
          }
       }
 
 sample_dict["class_b"] = {
-      "student1":{
-         "name":"Vasya",
-         "marks":{
-            "math":42,
-            "history":21
+      "student1": {
+         "name": "Vasya",
+         "marks": {
+            "math": 42,
+            "history": 21
          }
       }
    }
@@ -38,14 +38,17 @@ sample_dict['class_a']['student1']['marks']['physics'] = 15
 sample_dict['class_a']['student2']['marks']['physics'] = 67
 sample_dict['class_b']['student1']['marks']['physics'] = 77
 
+marks_number = len(sample_dict['class_a']['student1']['marks'])
+
+
 misha = list(sample_dict['class_a']['student1']['marks'].values())
 petya = list(sample_dict['class_a']['student2']['marks'].values())
 vasya = list(sample_dict['class_b']['student1']['marks'].values())
-misha = round((sum(misha) / 3), 2)
-petya = round((sum(petya) / 3), 2)
-vasya = round((sum(vasya) / 3), 2)
+misha = round((sum(misha) / marks_number), 2)
+petya = round((sum(petya) / marks_number), 2)
+vasya = round((sum(vasya) / marks_number), 2)
 
-print(misha, petya, vasya)
+print((misha, petya, vasya))
 
 # print(json.dumps(sample_dict, indent=2))
 # 1. Вывести значение ключа "name";
@@ -53,9 +56,11 @@ print(misha, petya, vasya)
 # 3. Добавить нового студента в "class_a", соответственно его "name" и "marks";
 # 4. Добавить новый класс со студентами;
 # 5. Добавить каждому студенту в "marks" предмет "physics" с оценкой;
-# 6. Подсчитать средний бал по каждому студенту (результат округлить до 2 знаков после запятой);
+# 6. Подсчитать средний бал по каждому студенту
+# (результат округлить до 2 знаков после запятой);
 # 7. Создать словарь со средним баллом за каждого студента;
 # 8. Определить лучшего студента по успеваемости;
-# 9. Подсчитать средний бал по каждому классу (результат округлить до 2 знаков после запятой);
+# 9. Подсчитать средний бал по каждому классу
+# (результат округлить до 2 знаков после запятой);
 # 8. Создать словарь со средним баллом за классы;
 # 9. Определить лучший класс по успеваемости.
