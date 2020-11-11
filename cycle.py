e85ab499cd0b0bd7191b8671ad0a623004b566d2
a.grinevich <Toha909570!>
2020-11-11 17:57:28 +0200

    Lab done

---
+++ cycle.py
@@ -0,0 +1,70 @@
+import string
+# i = 0
+# for i in range(101):
+#     print(i)
+#
+# i = 0
+# sum = 0
+# for i in range(1, 101):
+#     sum += i
+# print(sum)
+#
+# while i < 100:
+#     i += 1
+#     sum += i
+# print(sum)
+#
+# a, b = 9, 200
+#
+# even = [i for i in range(a, b) if i % 2 == 0]
+# odd = [i for i in range(a, b) if i % 2 == 1]
+#
+# # for i in range(a, b):
+# #     if i % 2 == 0:
+# #         even.append(i)
+# #     else:
+# #         odd.append(i)
+#
+#
+# print(even)
+# print(odd)
+# c = int(input())
+# #
+# a = [int(input('-')) for i in range(c)]
+# z = ''
+# for i in a:
+#     b = a.copy()
+#     b[b.index(i)] = -i
+#     for x in b:
+#         if x == i:
+#             z = 'yes'
+#     if z == 'yes':
+#         print('super')
+#         break
+#
+# f = 1
+# for i in range(1, int(input()) + 1):
+#     f = f * i
+# print(f)
+
+# a = int(input('-'))
+# d = {a: a**2 for a in range(2, a + 1)}
+# print(d)
+# letter = string.ascii_lowercase
+#
+# values = [letter[i] for i in range(len(letter))]
+# keys = [i + 1 for i in range(len(letter))]
+# # values = []
+# # keys = []
+# # for i in range(len(letter)):
+# #     values.append(letter[i])
+# #     keys.append((i + 1))
+# print(keys)
+# print(values)
+# Zip = zip(keys, values)
+# Dict = dict(Zip)
+# print(Dict)
+# Zip = zip(values, keys)
+# Dict = dict(Zip)
+# print(Dict)
+
